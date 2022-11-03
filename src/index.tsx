17c7dbbcc19fe93f73b13ba3b38b5d8a20dc2c1a
Nikita Sandakov <sunduckcow@gmail.com>
2022-11-03 17:49:30 +0300

    test commit 1

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//test1
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
